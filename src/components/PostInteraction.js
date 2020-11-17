f6c269d92a7f2beacb121433398b622c108fe2ba
erikcorea <erikcorrea444@gmail.com>
2020-11-17 14:32:59 -0600

    post-buttons

---
+++ src/components/PostInteraction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostInteraction = () => {
+    return (
+        <div>
+            <button>Like</button>
+            <button>comment</button>
+        </div>
+    );
+};
+
+export default PostInteraction;
