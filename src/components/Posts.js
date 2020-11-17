06c7cdfe11a81e7bfbfc67e31fb4739eb7e836f9
erikcorea <erikcorrea444@gmail.com>
2020-11-17 11:38:25 -0600

    first-post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Posts = () => {
+    // const [post, setPost] = useState();
+    // const url = `posts`
+
+    // useEffect(() => {
+    //     fetch(url)
+    //         .then((res) => res.json())
+    //         .then((resJson) => setPost(resJson))
+    //         .catch(console.error)
+    // }, []);
+
     return (
         <div>
-            
+            <h3>OMG my first message on Orange</h3>
         </div>
     );
 };
